f49b57568885b429e842cb54653da7f96271d1fd
ADITINAMDEO13 <aditinamdeo131199@gmail.com>
2021-02-18 23:08:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,45 @@ import './App.css';
 
 function App() {
   return (
+    <div className="container">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>THE FASHION LIBRARY</h1>
+      <p>Its a basic description over fashion library, where you can style yourself by your own choice and designs.<br></br>
+        you can choose your simplicity spice in your style ....<br></br>
+        Abhi bhi nahi samjhe?????<br></br>
+        okkkkkkk..... lemme take that responsibility to make you feel it.<br></br>
+        so in this site you will find your dream pair what makes you feel most exciting and lovable about what????....
+        YES about your clothings ...<br></br>
+        you are right, here you can tell us your favourable COLOR, DESIGN, PATTERN, STYLING, FITTING yes all in one place.<br></br>
+        so you have to give us brief details and then congrats soon you will get your love.<br></br>
+
+        so now we are here to make you beautiful as you are from inside by your soul.<br></br>
+        we will surely provide you suggestion as well according to your body type.<br></br>
+
+        we are here to help you out<br></br>
+        <h5>"dulaha ho ya baarati sb banege fashion ke khiladi,<br></br>
+        ladki wale dekhate rah jayege ace marege bazi"</h5> <br></br>
+
+        <h5>"dulhan hai sajana to banta hai"</h5><br></br>
+
+        <h5>"Arey aaj aap fir se nayi naveli lag rahi hai"</h5><br></br>
+
+        <h5>"Mera bhai kisi se kam hai kya"</h5><br></br>
+
+        <h5>"My father is looking most handsome man in the world in Fashion's style"</h5>
+        <br></br>
+        <h5>"Haaye tussi jachhh rahe ho"</h5>
+        <br></br>
+        <h5>"meri nanhi pari"</h5>
+        <br></br>
+
+        Yes guys and many more everything you will find in one place.
+        <br></br>
+        Thankyou
+        <br></br>
+
+      </p>
+    </div>
     </div>
   );
 }
